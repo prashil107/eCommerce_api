509234a192f080e667814bcb43ae3d488b9b13a0
prashil107 <prashilsingh107@gmail.com>
2025-03-24 21:14:28 +0530

    Documentation

--- ecommerce_api/server.js
+++ ecommerce_api/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require("path");
+const fs = require('fs'); // Import the file system module
 
 const app = express();
 connectDB(); // Ensure MongoDB is connected
@@ -10,12 +12,22 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('🚀 API is running...');
+app.get("/", (req, res) => {
+    fs.readFile(path.join(__dirname, "home.html"), 'utf8', (err, html) => { //changed index.html to home.html
+        if (err) {
+            console.error("Error reading home.html:", err); // Added error logging
+            return res.status(500).send('Error loading home.html');
+        }
+        res.send(html);
+    });
 });
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
